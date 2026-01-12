fbac3c703a9c8fc71ec05a66788fb7c8eed38f11
Ceasar Moya <ceasarmoya14@gmail.com>
2026-01-12 11:49:49 -0800

    Created a card manager script which holds all prefabs for all 52 cards in the deck. Also includes a facedown prefab.

---
+++ Assets/Scripts/CardManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] cardPrefabs;
+    [SerializeField] private GameObject hiddenCard;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+}
